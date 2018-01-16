a9571841bdb80f61410201cbd4bf6a674d5655eb
DonVeds <donveds@gmail.com>
2018-01-16 19:13:12 +0500

    Add pure MongoDB

--- controllers/buy.js
+++ controllers/buy.js
@@ -29,33 +29,38 @@ module.exports = {
             isTopic: true,
             topics: topics
           });
-        })
-    })
+        });
+    });
 
   },
 
 
   // GET /buy/topics/:topic
   showItemsByTopic(req, res) {
-    let topics = [];
-    for (item of items) {
-      if (topics.indexOf(item.topic) == -1) {
-        topics.push(item.topic)
-      }
-    }
+    connect().then(collection => {
+      collection.find().toArray()
+        .then(items => {
+          let topics = [];
+          for (item of items) {
+            if (topics.indexOf(item.topic) == -1) {
+              topics.push(item.topic)
+            }
+          }
 
-    let itemsByTopic = items.filter(item => item.topic.toLowerCase().replace(' ', '') == req.params.topic)
+          let itemsByTopic = items.filter(item => item.topic.toLowerCase().replace(' ', '') == req.params.topic)
 
-    let topicTitle = itemsByTopic[0].topic
+          let topicTitle = itemsByTopic[0].topic
 
-    res.render('buy/topics', {
-      items: itemsByTopic,
-      title: topicTitle,
-      name: req.name,
-      login: req.login,
-      password: req.password,
-      isTopic: true,
-      topics: topics
+          res.render('buy/topics', {
+            items: itemsByTopic,
+            title: topicTitle,
+            name: req.name,
+            login: req.login,
+            password: req.password,
+            isTopic: true,
+            topics: topics
+          });
+        });
     });
   },
 
@@ -63,62 +68,83 @@ module.exports = {
 
   // GET /buy/new
   showNew(req, res) {
-    newItems = [...items].sort((current, next) => next.year - current.year);
+    connect().then(collection => {
+      collection.find().toArray()
+        .then(items => {
+          newItems = [...items].sort((current, next) => next.year - current.year);
 
-    res.render('buy', { 
-      items: newItems, 
-      title: 'New', 
-      name: req.name, 
-      login: req.login, 
-      password: req.password,
-      isNew: true
+          res.render('buy', {
+            items: newItems,
+            title: 'New',
+            name: req.name,
+            login: req.login,
+            password: req.password,
+            isNew: true
+          });
+        });
     });
   },
 
   // GET /buy/top
   showTop(req, res) {
-    let topItems = items.filter(item => item.top);
+    connect().then(collection => {
+      collection.find().toArray()
+        .then(items => {
+          let topItems = items.filter(item => item.top);
 
-    res.render('buy', {
-      items: topItems,
-      title: 'Top',
-      name: req.name,
-      login: req.login,
-      password: req.password,
-      isTop: true
+          res.render('buy', {
+            items: topItems,
+            title: 'Top',
+            name: req.name,
+            login: req.login,
+            password: req.password,
+            isTop: true
+          });
+        });
     });
   },
 
   // GET /buy/sale 
   showSale(req, res) {
-    let saleItems = items.filter(item => item.sale);
+    connect().then(collection => {
+      collection.find().toArray()
+        .then(items => {
+          let saleItems = items.filter(item => item.sale);
 
-    res.render('buy', {
-      items: saleItems,
-      title: 'Sale',
-      name: req.name,
-      login: req.login,
-      password: req.password,
-      isSale: true,
+          res.render('buy', {
+            items: saleItems,
+            title: 'Sale',
+            name: req.name,
+            login: req.login,
+            password: req.password,
+            isSale: true,
+          });
+        });
     });
   },
 
 
   // GET /buy/:item
   showItem(req, res) {
-    for (item of items) {
-      if (req.params.item.toLowerCase().replace(' ', '') === item.title.toLowerCase().replace(' ', '')) {
-        req.item = [item];
-        console.log(req.item)
-      }
-    }
+    connect().then(collection => {
+      collection.find().toArray()
+        .then(items => {
+          for (item of items) {
+            if (req.params.item.toLowerCase().replace(' ', '') === item.title.toLowerCase().replace(' ', '')) {
+              req.item = [item];
+              console.log(req.item)
+            }
+          }
 
-    res.render('buy', {
-      items: req.item,
-      title: req.item[0].title,
-      name: req.name,
-      login: req.login,
-      password: req.password,
+          res.render('buy', {
+            items: req.item,
+            title: req.item[0].title,
+            name: req.name,
+            login: req.login,
+            password: req.password,
+          });
+        });
     });
-  },
+  }
+
 }
